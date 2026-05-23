7cd155d7633d84c0685a0c0dce33e62a29d12274
0xSec <0x.sec.aman@gmail.com>
2026-05-23 11:10:05 +0530

    Add User Interface and handling

--- tools/xdb.cpp
+++ tools/xdb.cpp
@@ -1,9 +1,64 @@
+#include <cstdio>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string_view>
 #include <iostream>
 #include <unistd.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <editline/readline.h>
+
+//handle user command
+namespace{
+    std::vector<std::string> split(std::string_view str, char delimiter){
+        std::vector<std::string> output{};
+        std::stringstream ss {std::string{str}};
+        std::string item;
+
+        while(std::getline(ss, item, delimiter)){
+            output.push_back(item);
+        }
+
+        return output;
+    }
+
+    bool is_prefix(std::string_view str, std::string_view of){
+        if(str.size() > of.size()) return false;
+        return std::equal(str.begin(), str.end(), of.begin());
+    }
+
+    void resume(pid_t pid){
+        if(ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0){
+            std::cerr << "Couldn't Continue\n" << std::endl;
+            std::exit(-1);
+        }
+    }
+
+    void wait_on_signal(pid_t pid){
+        int waitStatus;
+        int options = 0;
+        if(waitpid(pid, &waitStatus, options) < 0){
+            std::perror("waitpid failed");
+            std::exit(-1);
+        }
+    }
+
+    void HandleCommnd(pid_t pid, std::string_view line){
+        auto args = split(line, ' ');
+        auto command = args[0];
+
+        if(is_prefix(command, "continue")){
+            resume(pid);
+            wait_on_signal(pid);
+        }
+        else{
+            std::cerr << "Unknown Command\n" << std::endl;
+        }
+    }
+}
 
 namespace{
     pid_t attach(int argc, char* argv[]){
@@ -61,5 +116,29 @@ int main(int argc, char* argv[]){
     if(waitpid(pid, &waitStatus, options) < 0){
         std::perror("waitpid failed");
         return -1;
+    }
+
+    char* line = nullptr;
+    while((line = readline("xdb> ")) != nullptr){
+        /*
+         if user enters nothing it will execute previous command
+         */
+        std::string line_str;
+
+        if(line == std::string_view("")){
+            free(line);
+            if(history_length > 0){
+                line_str = history_list()[history_length - 1] -> line;
+            }
+        }
+        else{
+            line_str = line;
+            add_history(line);
+            free(line);
+        }
+
+        if(!line_str.empty()){
+            HandleCommnd(pid, line_str);
+        }
     }
 }
